6d04be78c61fe8e136e1ffd088c42b446d88c883
Fabio Biondi <info@fabiobiondi.com>
2026-01-07 02:38:19 +0100

    chore: Rename project from `angular-ai-simple-demo` to `angular-ai-gemini-masterclass-1` across configuration and test files.

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-ai-simple-demo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-ai-gemini-masterclass-1');
   });
 });
